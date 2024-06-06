c4266a0650d6ec15356950b41acdf189d79adde3
Lukecb14 <114123793+Lukecb14@users.noreply.github.com>
2024-06-06 14:48:22 -0400

    Moved footer, header & script into proper folders

--- scripts/loadNavAndFoot.js
+++ scripts/loadNavAndFoot.js
@@ -1,11 +1,11 @@
 window.onload = function() {
-    fetch('/navbar.html')
+    fetch('/html-elements/ navbar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
     });
 
-    fetch('/footer.html')
+    fetch('/html-elements/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
